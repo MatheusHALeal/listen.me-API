7c945d016917b1c13e79f269bd8fb398ee734e6a
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2019-03-30 19:56:38 -0300

    docker

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//Load express module with `require` directive
+const express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+//Launch listening server on port 8081
+app.listen(3000, function () {
+  console.log('app listening on port 8081!')
+})
+
